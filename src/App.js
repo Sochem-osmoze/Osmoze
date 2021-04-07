4dc268a6e58cf3053fc0830488a1bc8336337277
Rudresh Gupta <rudreshgpt7@gmail.com>
2021-04-07 09:51:58 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import './App.css';
 import Footer from './components/Footer'
 import Team from './components/Team'
-import Owlcarousel from './components/Owlcarousel'
 import HomePage from './components/Homepage'
 import Announcement from './components/Announcement'
 import {Switch, Route} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import NewNavBar from './components/newnavbar'
-import Header from './components/HeaderComponent'
 
 function App() {
   return (
       <div>
         {/* <Navbar /> */}
-        {/* <Header /> */}
         <NewNavBar />
         <Switch>
         <Route exact path = "/" component = {HomePage}/>
